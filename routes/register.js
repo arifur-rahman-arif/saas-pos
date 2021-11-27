b3eceb35098973be8a8ec1e39a9723566c6820d2
Arifur Rahman Arif <dragonwarrior052@gmail.com>
2021-11-27 23:24:15 +0600

    modified route error handler

---
+++ routes/register.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const chalk = require("chalk");
+
+const route = path.parse(path.basename(__filename)).name;
+
+router.get(`/${route}`, (req, res) => {
+    console.log(chalk.yellowBright.italic.bold(`Request for /api/${route}`));
+    res.status(201).send({
+        response: `Response from /api/${route}`,
+    });
+});
+
+module.exports = router;
